7e274362af2dbffbe65362dea3fcaed22cdbe914
Ronald Albor Cardenas <ronald.albor@gmail.com>
2022-06-23 11:11:53 -0500

    Conceptos basicos TypeScript

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+
+
+console.log('Hola Mundo!');
+
+/*
+    ===== Código de TypeScript =====
+*/
+// Definición de variables
+
+// definicion de string
+let cadena: string = "Esto es una cadena";
+
+console.log(cadena);
+
+// definicion de numero
+
+let numero: number = 12389;
+
+console.log(numero);
+
+// definicion de una variable combinada 
+
+let puedeSerCadenaOnumero: string | number = "Cadena";
+ 
+console.log(puedeSerCadenaOnumero);
+
+puedeSerCadenaOnumero = 120;
+
+console.log(puedeSerCadenaOnumero);
+
+// definicion de boolean
+
+let boolean: boolean = false;
+
+
