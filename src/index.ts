ef09ed7d3db63895b7ce0ee98bc6eccd84dab1b7
Ronald Albor Cardenas <ronald.albor@gmail.com>
2022-06-23 14:27:24 -0500

    conceptos basicos typeScript objetos, arreglos e interfaces

--- src/index.ts
+++ src/index.ts
@@ -33,3 +33,35 @@ console.log(puedeSerCadenaOnumero);
 let boolean: boolean = false;
 
 
+// definicion de Objetos, arreglos e interfaces
+
+//definicion de arreglo any 
+let arreglo1 = ['Cadena', 88, false];
+
+// definicion de arreglo string de
+
+let arreglo2: string[] = ['cadena','cadena2'];
+
+// definicion de arreglo tipo de datos combinado
+
+let arreglo3: (string | number | boolean)[] = ['cadena',2, true, false];
+
+//definicion de interface para Objetos
+interface Obj {
+    prop1:  string;
+    prop2:  number;
+    prop3:  string[];
+    prop4?: string;
+}
+
+//definicion de un objeto con interface
+
+const objeto1: Obj = {
+    prop1: "cadena",
+    prop2: 10,
+    prop3:['cadena2','cadena3']
+}
+
+objeto1.prop4 = 'propiedad 4';
+
+console.table(objeto1);
