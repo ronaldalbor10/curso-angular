63a5f82d69f44148285c399d1733390d5346a486
Ronald Albor Cardenas <ronald.albor@gmail.com>
2022-06-24 10:33:38 -0500

    Funciones con objetos con argumentos, tarea de interfaces

--- src/index.ts
+++ src/index.ts
@@ -109,3 +109,32 @@ const nuevoPersonaje: Personaje = {
 sanar(nuevoPersonaje,20);
 
 nuevoPersonaje.mostrarVida();
+
+interface Direccion {
+    calle: string;
+    pais: string;
+    ciudad: string;
+}
+
+interface SuperHeroe {
+    nombre: string;
+    edad: number;
+    direccion: Direccion;
+    mostrarDireccion: () => string;
+}
+
+const superHeroe: SuperHeroe = {
+    nombre:"Spiderman",
+    edad: 30,
+    direccion:{
+        calle:'Main St.',
+        pais: 'USA',
+        ciudad: 'NY'
+    },
+    mostrarDireccion(){
+        return this.nombre + ', ' + this.direccion.ciudad + ', ' +this.direccion.pais;
+    }
+}
+
+const direccion = superHeroe.mostrarDireccion();
+console.log(direccion);
