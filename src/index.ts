52102f0daf37487eb3feb02744fd9c6c2050967a
Ronald Albor Cardenas <ronald.albor@gmail.com>
2022-06-24 16:01:20 -0500

    Desestructuracion de objetos y arreglos

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ console.log('Hola Mundo!');
 /*
     ===== Código de TypeScript =====
 */
+
+//Clase 14 Tipos de datos y conceptos generales
+
+
 // Definición de variables
 
 // definicion de string
@@ -32,6 +36,7 @@ console.log(puedeSerCadenaOnumero);
 
 let boolean: boolean = false;
 
+//Clase 15 Objetos arreglos e interfaces
 
 // definicion de Objetos, arreglos e interfaces
 
@@ -66,6 +71,8 @@ objeto1.prop4 = 'propiedad 4';
 
 console.table(objeto1);
 
+//Clase 16 Funciones 
+
 // declaracion de funciones
 
 function sumar(a: number, b: number):number{
@@ -84,6 +91,8 @@ function multiplicar(a: number, b?: number ,c: number = 10){
 const resultado2 = multiplicar(10,30);
 
 console.log(resultado2);
+
+//Clase 17 Funciones on objetos y argumentos
 
 interface Personaje {
     nombre: string;
@@ -109,6 +118,8 @@ const nuevoPersonaje: Personaje = {
 sanar(nuevoPersonaje,20);
 
 nuevoPersonaje.mostrarVida();
+
+//Clase 18 Tarea sobre objetos e interfaces
 
 interface Direccion {
     calle: string;
@@ -138,3 +149,54 @@ const superHeroe: SuperHeroe = {
 
 const direccion = superHeroe.mostrarDireccion();
 console.log(direccion);
+
+// Clase 19 Desestructuración
+
+interface Reproductor {
+    volumen: number;
+    segundo: number;
+    cancion: string;
+    detalles: Detalles;
+
+}
+
+interface Detalles {
+    autor:string;
+    anio:number;
+}
+
+const reproductor: Reproductor = {
+    volumen:12,
+    segundo:20,
+    cancion:'El triste',
+    detalles: {
+        autor:'Hector Lavoe',
+        anio: 1970
+    }
+}
+
+// Desestructuración en una sola linea
+//const {cancion, detalles:{ autor, anio} } =  reproductor ;
+
+// Desestructuración recomendada
+const {cancion, detalles} =  reproductor ;
+//const {autor, anio } = detalles;
+
+//cambio de nombre de variable en Destructuración
+const {autor:autorDetalle, anio } = detalles;
+
+console.log('La cancion seleccionada es:',cancion);
+console.log('El autor de la cancion es:',autorDetalle);
+console.log('La cancion es del año:',anio);
+
+// Clase 20 Desestructuración de arreglos 
+
+const lenguajes: string[] = ["Java", "PHP", "C++"];
+
+//para destrcturar arreglos se realiza con [] y las variables se asignan por el orden del arreglo
+
+const [] = lenguajes;
+console.log('El lenguaje 1 es:',lenguajes[0]);
+console.log('El lenguaje 2 es:',lenguajes[1]);
+console.log('El lenguaje 3 es:',lenguajes[2]);
+
